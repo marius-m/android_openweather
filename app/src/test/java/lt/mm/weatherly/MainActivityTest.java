d47ecfeaf472e29f9e4b88e8ee01141c59a8a855
Marius Merkevičius <marius.m@ito.lt>
2015-08-21 21:21:56 +0300

    Initialized project testing environment. Removed unnecessary methods.

---
+++ app/src/test/java/lt/mm/weatherly/MainActivityTest.java
@@ -0,0 +1,19 @@
+package lt.mm.weatherly;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mariusmerkevicius on 8/21/15.
+ */
+@RunWith(RobolectricTestRunner.class)
+public class MainActivityTest {
+    @Test
+    public void testName() throws Exception {
+        assertTrue(true);
+
+    }
+}
